eb9d495deb51d84c488a86d5c9f848d43a7287c5
Tan Wei Lian <twl_william89@hotmail.com>
2019-09-22 12:43:52 +0800

    fix no module google colab

--- inceptionNetV3-test.py
+++ inceptionNetV3-test.py
@@ -25,7 +25,6 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.applications.inception_v3 import InceptionV3
-from google.colab import drive
 import os
 
 
