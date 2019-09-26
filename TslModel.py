78e4e7fb929c1c0177f5c0475dc9206bcbe10d3c
wliant <tanweilian@outlook.sg>
2019-09-26 20:15:51 +0800

    v3 model

--- TslModel.py
+++ TslModel.py
@@ -26,4 +26,29 @@ def TslNetV1(input_shape, no_of_class):
     model = Model(inputs=visible, outputs=layer)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
+    
+def TslNetV3(input_shape, no_of_class):
+    visible = Input(shape=input_shape)
+    layer = Conv2D(32, (3,3), padding='same', activation='relu')(visible)
+    layer = Conv2D(32, (3,3), padding='same', activation='relu')(layer)
+    layer = MaxPooling2D((2,2), strides=(2,2))(layer)
+    layer = Conv2D(64, (3,3), padding='same', activation='relu')(layer)
+    layer = Conv2D(64, (3,3), padding='same', activation='relu')(layer)
+    layer = MaxPooling2D((2,2), strides=(2,2))(layer)
+    layer = Conv2D(128, (3,3), padding='same', activation='relu')(layer)
+    layer = Conv2D(128, (3,3), padding='same', activation='relu')(layer)
+    layer = MaxPooling2D((2,2), strides=(2,2))(layer)
+    layer = Conv2D(256, (3,3), padding='same', activation='relu')(layer)
+    layer = Conv2D(256, (3,3), padding='same', activation='relu')(layer)
+    layer = MaxPooling2D((2,2), strides=(2,2))(layer)
+    layer = Conv2D(512, (3,3), padding='same', activation='relu')(layer)
+    layer = Conv2D(512, (3,3), padding='same', activation='relu')(layer)
+    layer = MaxPooling2D((2,2), strides=(2,2))(layer)
+    layer = Flatten()(layer)
+    layer = Dense(1024, activation='relu')(layer)
+    layer = Dense(1024, activation='relu')(layer)
+    layer = Dense(no_of_class, activation='softmax')(layer)
 
+    model = Model(inputs=visible, output=layer)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
